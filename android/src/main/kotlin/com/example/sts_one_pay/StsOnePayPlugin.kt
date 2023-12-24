2e48fa0352ba02d6e41ba04a41452949d061b6bf
Mohammad Zeino <105930030+mohammadz0606@users.noreply.github.com>
2023-12-24 18:23:05 +0300

    solve bug in tokens list

--- android/src/main/kotlin/com/example/sts_one_pay/StsOnePayPlugin.kt
+++ android/src/main/kotlin/com/example/sts_one_pay/StsOnePayPlugin.kt
@@ -7,6 +7,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry
 
 /** StsOnePayPlugin */
 class StsOnePayPlugin: FlutterPlugin, MethodCallHandler {
@@ -32,4 +33,5 @@ class StsOnePayPlugin: FlutterPlugin, MethodCallHandler {
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
+
 }
